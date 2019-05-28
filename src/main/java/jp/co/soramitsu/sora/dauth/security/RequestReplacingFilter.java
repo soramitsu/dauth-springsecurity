73721b542e3b55bca3fe67edbe4ae6527ed78dc8
jiexa <malyshevalexey8@gmail.com>
2019-05-28 17:41:17 +0300

    fix filter for multipart requests

--- src/main/java/jp/co/soramitsu/sora/dauth/security/RequestReplacingFilter.java
+++ src/main/java/jp/co/soramitsu/sora/dauth/security/RequestReplacingFilter.java
@@ -1,5 +1,7 @@
 package jp.co.soramitsu.sora.dauth.security;
 
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,6 +17,11 @@ public class RequestReplacingFilter extends OncePerRequestFilter {
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
+    if (request.getHeader(CONTENT_TYPE).startsWith("multipart")) {
+      log.trace("Passing request without changes");
+      filterChain.doFilter(request, response);
+      return;
+    }
     log.trace("Replacing request with wrapper");
     filterChain.doFilter(new RequestWrapper(request), response);
   }
